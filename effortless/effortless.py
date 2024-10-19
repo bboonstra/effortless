e0ddddae5ecaf8b99f6f3f797270a78342dbbbc8
Ben Boonstra <bboonstra26@gmail.com>
2024-10-19 01:11:33 -0500

    Release candidate 1 [test]

--- effortless/effortless.py
+++ effortless/effortless.py
@@ -1,11 +1,18 @@
 # effortlessdb/effortless.py
 
-_storage = {}
+import json
+import os
+
+_STORAGE_FILE = 'db.effortless'
 
 def set(obj):
-    """Store an object in a simple storage."""
-    _storage['object'] = obj
+    """Store an object in a persistent file-based storage."""
+    with open(_STORAGE_FILE, 'w') as f:
+        json.dump(obj, f)
 
 def get():
-    """Retrieve the stored object."""
-    return _storage.get('object', None)
+    """Retrieve the stored object from file-based storage."""
+    if not os.path.exists(_STORAGE_FILE):
+        return None
+    with open(_STORAGE_FILE, 'r') as f:
+        return json.load(f)
